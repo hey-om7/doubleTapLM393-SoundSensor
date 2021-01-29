0db80ee5cfee5f9461afe5f4b72ddabaea9db9a5
Om Ambarkar <66007853+hey-om7@users.noreply.github.com>
2021-01-29 11:03:41 +0530

    All unused comments are being removed

--- lm393_sound_sensor.ino
+++ lm393_sound_sensor.ino
@@ -6,7 +6,7 @@
 */
 
 void setup() {
-  // put your setup code here, to run once:
+
 Serial.begin(9600);
 pinMode (5, INPUT);
 }
@@ -14,9 +14,8 @@ pinMode (5, INPUT);
 void loop() {
  
   if(digitalRead(5)==1){
-//  Serial.println("clapped");
+//  Serial.println("single clapped");
   delay(400);
-//  Serial.println(digitalRead(5));
 for (int i=0;i<1100;i++){
     if(digitalRead(5)==1){
     Serial.println("Double Clap Detected");
@@ -26,12 +25,7 @@ for (int i=0;i<1100;i++){
     }
     delay(1);
   }
-
-  }
-
-//    if(digitalRead(5)==1){
-//  Serial.println(digitalRead(5));
-//    }
+    }
 delay(1);
 
 }
