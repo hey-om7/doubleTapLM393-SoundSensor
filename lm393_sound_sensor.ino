79a4878864a76bb399a1dfd4de1c4983776af840
Om Ambarkar <66007853+hey-om7@users.noreply.github.com>
2021-01-29 11:32:05 +0530

    Sensor's Clap Detection increased significantly
    Efficient code to detect double claps

--- lm393_sound_sensor.ino
+++ lm393_sound_sensor.ino
@@ -1,31 +1,44 @@
 /*
   Created by Om Ambarkar
-  on 28Jan2021
+  on 29Jan2021
   LM393 Sound Sensor
 
 */
 
 void setup() {
-
 Serial.begin(9600);
 pinMode (5, INPUT);
 }
 
+int clap(){
+  return digitalRead(5);
+  }
+
+ void print(String text){
+  Serial.println(text);
+  }
+
 void loop() {
  
-  if(digitalRead(5)==1){
-//  Serial.println("single clapped");
-  delay(400);
-for (int i=0;i<1100;i++){
-    if(digitalRead(5)==1){
-    Serial.println("Double Clap Detected");
-    delay(1000);
+  if(clap()==1){
+//  Serial.println("Single Clapped");
+  delay(100);
+for (int j=0;j<1100;j++){
+  if(clap()==0){
+    for (int i=0;i<1100;i++){
+      if(clap()==1){
+        
+        Serial.println("Double Clapped");
+        delay(400);
+        break;
+        }
+        delay(1);
+           }
     break;
-    
-    }
-    delay(1);
+      }
+  delay(1);
+     }
   }
-    }
 delay(1);
 
 }
